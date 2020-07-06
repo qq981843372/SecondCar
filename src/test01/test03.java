bb9a5e333a2ee839b4e03009354f2271edba0d22
qq981843372 <981843372@qq.com>
2020-07-06 15:04:29 +0800

    101010

--- src/test01/test03.java
+++ src/test01/test03.java
@@ -1,10 +1,10 @@
 package test01;
 
-public class test011 {
+public class test03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1233453456786786");
+		System.out.println("ÄÚÈİ");
 	}
 
 }
